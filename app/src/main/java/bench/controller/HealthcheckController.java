4562110d304d485a7773a84cd10505f96c50e461
kb-pk <209384531+kb-pk@users.noreply.github.com>
2026-05-10 14:37:14 +0200

    feat: added spring boot stub

---
+++ app/src/main/java/bench/controller/HealthcheckController.java
@@ -0,0 +1,14 @@
+package bench.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HealthcheckController {
+    @GetMapping("/healthcheck")
+    public Map<String, String> getHealthcheck() {
+        return Map.of("status", "ok");
+    }
+}
